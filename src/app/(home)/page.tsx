fdc8681c289bb64ef9275a70292784b5afd5ae35
betteree <kkne1017@gmail.com>
2025-03-24 10:47:33 +0900

    nextjs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,8 @@ export const metadata = {
   title: "Home",
 };
 
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL =
+  "https://nomad-movies.nomadcoders.workers.dev/movies" as string;
 
 async function getMovies() {
   // await new Promise((resolve) => setTimeout(resolve, 1000));
--- src/components/movie-info.tsx
+++ src/components/movie-info.tsx
@@ -1,7 +1,7 @@
 import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   console.log(`Fetching movies: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(`${API_URL}/${id}`);
@@ -19,7 +19,7 @@ export default async function MovieInfo({ id }: { id: string }) {
       />
       <div className={potato.info}>
         <h1 className={potato.title}>{movie.title}</h1>
-        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <h3>⭐️ {movie.vote_average.toFixed()}</h3>
         <p>{movie.overview}</p>
         <a href={movie.homepage} target={"_blank"}>
           Homepage &rarr;
