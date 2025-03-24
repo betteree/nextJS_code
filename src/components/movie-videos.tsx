c88aff1d4dc6a5d8f5b2a59dce029c8e5a33b0a1
betteree <kkne1017@gmail.com>
2025-03-24 10:10:52 +0900

    4.2 Movie Styles

--- src/components/movie-videos.tsx
+++ src/components/movie-videos.tsx
@@ -1,9 +1,6 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("something broke...");
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
