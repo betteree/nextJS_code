d3f87be15ee00c4705d544c859c44309af3f8d48
betteree <kkne1017@gmail.com>
2025-04-02 17:39:15 +0900

    드래그 이벤트 추가

--- src/components/coachPlayer.tsx
+++ src/components/coachPlayer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import styles from "@/styles/coachBoard.module.css";
 
 export default function CoachPlayer() {
   const [items, setItems] = useState([
@@ -10,8 +11,27 @@ export default function CoachPlayer() {
     "최민수",
     "정하늘",
   ]);
+  const [draggedIndex, setDraggedIndex] = useState(null);
+
+  const handleDragStart = (index) => {
+    setDraggedIndex(index);
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLLIElement>) => {
+    e.preventDefault();
+    // 기존동작 방지해서 드롭이 가능하도록 한다
+  };
+
+  const handleDrop = (index) => {
+    const newItems = [...items];
+    const [removed] = newItems.splice(draggedIndex, 1);
+    newItems.splice(index, 0, removed);
+
+    setItems(newItems);
+    setDraggedIndex(null);
+  };
   return (
-    <div>
+    <div className={styles.playerContainer}>
       <header>
         <h2>선수 등록</h2>
         <span>
@@ -22,13 +42,56 @@ export default function CoachPlayer() {
         </span>
         <button>추가</button>
       </header>
-
-      <section>
-        <ul>
-          {items.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
+      <button>배치</button>
+      <section className={styles.playerList}>
+        <div className={styles.partContainer}>
+          <h3>도마 순서</h3>
+          <ul>
+            {items.map((name, index) => (
+              <li
+                key={index}
+                draggable
+                onDragStart={() => handleDragStart(index)}
+                onDragOver={handleDragOver}
+                onDrop={() => handleDrop(index)}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>철봉 순서</h3>
+          <ul>
+            {items.map((name, index) => (
+              <li
+                key={index}
+                draggable
+                onDragStart={() => handleDragStart(index)}
+                onDragOver={handleDragOver}
+                onDrop={() => handleDrop(index)}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>평행봉 순서</h3>
+          <ul>
+            {items.map((name, index) => (
+              <li
+                key={index}
+                draggable
+                onDragStart={() => handleDragStart(index)}
+                onDragOver={handleDragOver}
+                onDrop={() => handleDrop(index)}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </div>
   );
