13eaa0b9da9fa4769bcfdea9c4d938daabf8dd0d
betteree <kkne1017@gmail.com>
2025-04-04 17:50:02 +0900

    지도자 h2 태그 추가

--- src/app/coach_board/page.tsx
+++ src/app/coach_board/page.tsx
@@ -6,6 +6,9 @@ import CoachPlayer from "@/components/coach/coachPlayer";
 export default function Contest() {
   return (
     <div className={styles.container}>
+      <nav>
+        <h2>지도자</h2>
+      </nav>
       <CoachPlayer></CoachPlayer>
     </div>
   );
