cb7117951344647a4e918a0a78b9692f2cca72d6
betteree <kkne1017@gmail.com>
2025-05-28 14:13:57 +0900

    줌 확대 기능

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko-KR">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;" /> 
       </head>
       <body>
         <ThemeRegistry>
