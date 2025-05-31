dc629491e5b2516b7db6c89417d814d4ba855dbf
betteree <kkne1017@gmail.com>
2025-05-31 13:42:43 +0900

    코치 이름 첫글자 대문자

--- src/components/coach/coachInfo.tsx
+++ src/components/coach/coachInfo.tsx
@@ -6,7 +6,7 @@ import { Coach } from "@/types/player";
 import Image from 'next/image';
 import { getCodeByName } from "../data/country/countryList";
 import Flag from "react-world-flags";
-
+import { capitalizeWords } from "./capitalWords/capitalWords";
 
 export default function CoachInfo({dict}:{dict:Record<string, string>}) {
   const [coachData, setCoachData] = useState<Coach | null>(null);
@@ -35,7 +35,7 @@ export default function CoachInfo({dict}:{dict:Record<string, string>}) {
     <Paper elevation={3} sx={{ padding: 3, margin: 1 , boxShadow:"0"}}>
       <Typography variant="h5" fontWeight={600} color="black" sx={{ display:"flex" ,gap:2, alignItems:"center", mb: 3 }} gutterBottom>
          <Image src="/icon/info.png" alt="info" width={35} height={35} />
-          {coachData?.name}
+          {capitalizeWords(coachData?.name ?? '')}
       </Typography>
 
       {coachData ? (
